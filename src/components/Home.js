00211619ca06c1f6846b819096fa25522c7cd7a7
shrikant <shrikanthshivangi999@gmail.com>
2024-01-16 10:48:18 +0530

    slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
-import { Card, Carousel } from 'react-bootstrap'
+import { Carousel } from 'react-bootstrap'
 import img1 from '../images/79_inr.jpg'
 import img2 from '../images/83_inr.jpg'
 import img3 from '../images/85_inr.jpg'
 import { Toaster, toast } from 'react-hot-toast'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
+import {Card} from 'react-bootstrap'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
@@ -13,8 +14,71 @@ import { useSelector } from 'react-redux'
 const Home = () => {
     const navigate = useNavigate()
     const location = useLocation()
+
     console.log('hhh',location.state)
     // alert('lhjkhhj')
+    const handleClick = (id) => {
+        navigate(`/product/${id}`);
+      };
+    function Arrow(props) {
+        const { className, style, onClick } = props;
+        return (
+          <div
+            className={className}
+            style={{ ...style, display: "block", background: "black" }}
+            onClick={onClick}
+          />
+        );
+      }
+      function SamplePrevArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+          <div
+            className={className}
+            style={{ ...style, display: "block", background: "black" }}
+            onClick={onClick}
+          />
+        );
+      }
+
+    const products = useSelector(state=>state.products.data)
+    const newArrivals = products.slice((50*products.length)/100)
+    var settings = {
+        dots: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        nextArrow: <Arrow />,
+        prevArrow: <SamplePrevArrow />,
+        infinite:true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
 
     useEffect(() =>{
         if(location.state?.msg){
@@ -22,11 +86,11 @@ const Home = () => {
             toast.success('Logged In')
         }
     },[])
-    let newProducts = products.slice((90*products.length)/100)
-    console.log('ng',newProducts)
+
+    console.log('ng')
 
     return (
-        <div style={{minHeight:'82%'}} >
+        <div style={{minHeight:'82%',display:'flex',flexDirection:'column'}} >
             <Toaster/>
             <Carousel>
                 <Carousel.Item>
@@ -65,7 +129,22 @@ const Home = () => {
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-
+            <h2 style={{display:'inline-block',margin:'50px auto'}}>New Arrivals</h2>
+            <Slider {...settings} style={{width:'90vw',margin:'0 auto'}}>
+               {newArrivals.map(ele=>{
+                return (<Card style={{margin:'0 10px'}} onClick={()=>{handleClick(ele._id)}}>
+                <Card.Img
+                  className="custom-card-img"
+                  src={ele.image[0].url}
+                  key={ele.image[0].key}
+                />
+                <Card.Body>
+                  <Card.Title>{ele.title}</Card.Title>
+                  <Card.Text>₹{ele.price}</Card.Text>
+                </Card.Body>
+              </Card>)
+               })} 
+            </Slider>
         </div>
     )
 }
