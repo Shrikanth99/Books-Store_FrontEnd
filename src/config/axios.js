01a67f85de809b5b0503d70a5863ac7fc86b9a21
shrikant <shrikanthshivangi999@gmail.com>
2024-02-20 00:22:52 +0530

    api-updated

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:3055'
+    baseURL : 'https://books-store-api-z5zj.onrender.com'
+    // baseURL: 'http://localhost:3055'
 })
